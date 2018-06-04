2e32d692d787c7bcc283a8b58b2bd7c6ab5c5e99
Muhammad Andry Kurniawan <36904323+MAKEvo@users.noreply.github.com>
2018-06-04 09:19:19 +0700

    Dokumen klasifikasi

--- dokumen klasifikasi/doklas.R
+++ dokumen klasifikasi/doklas.R
@@ -1,8 +1,34 @@
+library(caret)
 library(tm)
-library(plyr)
-library(class)
-libs=c("tm","plyr","class")
-lapply(libs,require,character.only = T)
-options(stringsAsFactors= F)
-cat = c("Oneliner.csv","Sickness.csv","Complaint.csv","ATC.csv","Availabilty.csv","Oneliner.csv")
-pathname = "C:/Users/s769346/Desktop/New folder"
+library(SnowballC)
+library(arm)
+# Training data.
+cname <- file.path("/resources/data/document")   
+cname   
+dir(cname)  
+docs <- Corpus(DirSource(cname))   
+
+summary(docs)
+inspect(docs[2])
+docs <- tm_map(docs, removePunctuation)  
+docs <- tm_map(docs, removeNumbers)  
+docs <- tm_map(docs, tolower)  
+docs <- tm_map(docs, removeWords, c("dapat", "yang","adalah","untuk","dan"))  
+corpus <- VCorpus(VectorSource(data))
+
+# Create a document term matrix.
+tdm <- DocumentTermMatrix(corpus, list(removePunctuation = TRUE, stopwords = TRUE, stemming = TRUE, removeNumbers = TRUE))
+
+# Convert to a data.frame for training and assign a classification (factor) to each document.
+train <- as.matrix(tdm)
+train <- cbind(train, c(0, 1))
+colnames(train)[ncol(train)] <- 'y'
+train <- as.data.frame(train)
+train$y <- as.factor(train$y)
+data
+train
+# Train.
+fit <- train(y ~ ., data = train, method = 'bayesglm')
+
+# Check accuracy on training.
+predict(fit, newdata = train)
